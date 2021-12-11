d640d43827aa16024e275d1ce753aa1700afa43a
zguan20 <zguan20@uic.edu>
2021-12-11 17:00:02 -0600

    小小进步

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ import enum
 client = discord.Client()
 
 
+
+import time
+
+
+def countdown(num_of_secs):
+    while num_of_secs:
+        m, s = divmod(num_of_secs, 60)
+        min_sec_format = '{:02d}:{:02d}'.format(m, s)
+        print(min_sec_format)
+        time.sleep(1)
+        num_of_secs -= 1
+
 class id(enum.Enum):
     wolf = 1
     civilian = 2
